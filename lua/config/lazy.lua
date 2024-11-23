d5afcdc6156aefbfd8e460087350424a01f8a37c
Vitalii Goncharov <1c.audit@gmail.com>
2024-11-23 18:37:17 +0200

    wip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "plugins.extras.copilot-chat-v2" },
+    -- { import = "plugins.extras.copilot-chat-v2" },
     -- { import = "lazyvim.plugins.extras.lang.java" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
