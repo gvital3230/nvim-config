424e5a8ab7992262ba9d33d1001d95ccf078aeca
Vitalii Goncharov <1c.audit@gmail.com>
2025-03-08 20:05:37 +0200

    (terraform) change root_spec
    - change root_spec to let open neotree in current folder

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,7 @@
 -- LSP Server to use for PHP.
 -- Set to "intelephense" to use intelephense instead of phpactor.
 vim.g.lazyvim_php_lsp = "intelephense"
+
+-- set the root spec to cwd
+-- https://github.com/LazyVim/LazyVim/discussions/2150#discussioncomment-7787591
+vim.g.root_spec = { "cwd" }
