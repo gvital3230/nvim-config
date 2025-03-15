79538c6140372f14162d9a6f67efa4107b273094
Vitalii Goncharov <1c.audit@gmail.com>
2025-03-15 18:18:30 +0200

    (copilot) change settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,6 @@ vim.g.lazyvim_php_lsp = "intelephense"
 -- set the root spec to cwd
 -- https://github.com/LazyVim/LazyVim/discussions/2150#discussioncomment-7787591
 vim.g.root_spec = { "cwd" }
+
+-- https://github.com/LazyVim/LazyVim/discussions/5207#discussioncomment-12312625
+vim.g.ai_cmp = false
